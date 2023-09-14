57d9e57a26d48ffa273c6f1fc7358641959f7e1c
ratchetandcunt <42insidethebox@gmail.com>
2023-09-14 15:08:28 +0200

    Section 20 of the course Angular - The Complete Guide

---
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-loading-spinner",
+  template: `<div class="loadingio-spinner-spinner-p89v3x99f1">
+    <div class="ldio-hj3lz9ipgv4">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>`,
+  styleUrls: ["./loading-spinner.component.css"],
+})
+export class LoadingSpinnerComponent {}
