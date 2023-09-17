5e1c1a30d54e1eeff8c5caa9f8e27b6c970144ab
ratchetandcunt <42insidethebox@gmail.com>
2023-09-17 14:03:11 +0200

    Angular Core and Angular Cli updated to v10

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 
 // @Injectable({ providedIn: "root" })
+@Injectable()
 export class LoggingService {
   lastLog: string;
   printLog(message: string) {
